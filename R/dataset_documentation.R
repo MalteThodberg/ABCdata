5210b3b0517fdd6eaa942dd70a8aebe093635a3c
MalteThodberg <maltethodberg@gmail.com>
2015-10-27 15:36:24 +0100

    documentation for datasets

--- R/dataset_documentation.R
+++ R/dataset_documentation.R
@@ -39,3 +39,45 @@
 #' }
 #' @source \url{http://www.bioconductor.org/packages/release/data/experiment/html/zebrafishRNASeq.html}
 "Zebrafish"
+
+#' Rat neuron experiment
+#'
+#' RNA-Seq data originally from RECount. For more information, see reference from URL.
+#'
+#' @format List with 3 elements:
+#' \describe{
+#'   \item{Expression}{Integer matrix: Features by samples matrix with integers representing raw counts}
+#'   \item{Design}{Dataframe: Study design}
+#'   \item{Annotation}{Dataframe: Additional info about features}
+#'   The three objects are already in the same order, but can be matched using row/column names.
+#' }
+#' @source \url{http://www.ncbi.nlm.nih.gov/pubmed?term=20452967}
+"RatNeurons"
+
+#' Human Tissue Experiment (Wang et al.)
+#'
+#' RNA-Seq data originally from RECount. For more information, see reference from URL.
+#'
+#' @format List with 3 elements:
+#' \describe{
+#'   \item{Expression}{Integer matrix: Features by samples matrix with integers representing raw counts}
+#'   \item{Design}{Dataframe: Study design}
+#'   \item{Annotation}{Dataframe: Additional info about features}
+#'   The three objects are already in the same order, but can be matched using row/column names.
+#' }
+#' @source \url{http://www.ncbi.nlm.nih.gov/pubmed?term=18978772}
+"WangTissues"
+
+#' Human Tissue Experiment (Asmann et al.)
+#'
+#' RNA-Seq data originally from RECount. For more information, see reference from URL.
+#'
+#' @format List with 3 elements:
+#' \describe{
+#'   \item{Expression}{Integer matrix: Features by samples matrix with integers representing raw counts}
+#'   \item{Design}{Dataframe: Study design}
+#'   \item{Annotation}{Dataframe: Additional info about features}
+#'   The three objects are already in the same order, but can be matched using row/column names.
+#' }
+#' @source \url{http://www.ncbi.nlm.nih.gov/pubmed/22496456}
+"AsmannTissues"
